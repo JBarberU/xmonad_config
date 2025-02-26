401a871adffa75f4c927c214dccd4da0dc7cb5b2
John Barbero Unenge <git@lsrkttn.com>
2025-02-26 08:06:41 +0100

    [shared] Change colors

--- src/MyColors.hs
+++ src/MyColors.hs
@@ -12,44 +12,48 @@ module MyColors
         , myLightRed
         , myMediumBeige
         , myRed
+        , myTransparent
         , myVeryDarkGrey
     ) where
-
-myRed :: String
-myRed = "#"
-
-myLightRed :: String
-myLightRed = "#fb4934"
-
-myDarkRed :: String
-myDarkRed = "#cc241d"
-
-myDarkPurple :: String
-myDarkPurple = "#b16286"
-
-myLightPurple :: String
-myLightPurple = "#d3869b"
-
-myDarkGrey :: String
-myDarkGrey = "#1d2021"
-
-myLightGreen :: String
-myLightGreen = "#b8bb26"
-
-myLightBlue :: String
-myLightBlue = "#83a598"
 
 myDarkBlue :: String
 myDarkBlue = "#458588"
 
-myMediumBeige :: String
-myMediumBeige = "#a89984"
+myDarkGrey :: String
+myDarkGrey = "#1d2021"
 
-myVeryDarkGrey :: String
-myVeryDarkGrey= "#282828"
+myDarkPurple :: String
+myDarkPurple = "#b16286"
+
+myDarkRed :: String
+myDarkRed = "#cc241d"
 
 myLightBeige :: String
 myLightBeige= "#ebdbb2"
 
+myLightBlue :: String
+myLightBlue = "#83a598"
+
+myLightGreen :: String
+myLightGreen = "#b8bb26"
+
 myLightGrey :: String
 myLightGrey = "#928374"
+
+myLightPurple :: String
+myLightPurple = "#d3869b"
+
+myLightRed :: String
+myLightRed = "#fb4934"
+
+myMediumBeige :: String
+myMediumBeige = "#a89984"
+
+myRed :: String
+myRed = "#"
+
+myTransparent :: String
+myTransparent = "#00000000"
+
+myVeryDarkGrey :: String
+myVeryDarkGrey= "#282828"
